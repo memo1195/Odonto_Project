22b88fac1c79c7da13d971703878301603f73370
Carlos Catalan <carloscatalan94c@gmail.com>
2020-08-04 19:39:01 -0700

    Fix JS master

--- static/js/main.js
+++ static/js/main.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 $("#button-modal").click(function(){
     //  Nombre
     let namef1 = document.getElementById("namef1");
-    let namef2 = document.getElementById("name");
+    let namef2 = document.getElementById("nombre_paciente");
     namef2.value = namef1.value;
 
     //  Tratamiento
     let t1 = document.getElementById("treatf1");
-    let t2 = document.getElementById("treat");
+    let t2 = document.getElementById("tratamiento_id");
     t2.value = t1.value;
 });
 
